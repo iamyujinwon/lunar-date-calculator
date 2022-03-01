81c9cdb6b3bb03b1718a413992b2687fdab8d3d6
Yujin Won <iamyujinwon@gmail.com>
2022-03-01 16:26:27 -0500

    change variable names and edit function logic

--- server.js
+++ server.js
@@ -2,48 +2,80 @@ const monthsInString = ["January", "February", "March", "April", "May", "June", 
 // const allDaysInLunarYear = 354;
 
 const prompt = require('prompt-sync')();
+const ordinal = require('ordinal');
 
 const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth(); // month + 1 = current month HOWEVER! I need and use only index number so do not need to add 1 to it
+const currentDay = new Date().getDay();
 const solarDaysForEachMonthInCurrentYear = [ 31, 28,  31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 const lunarDaysForEachMonthInCurrentYear = [ 30, 29, 30, 29, 30, 30, 29, 30, 29, 30, 29, 30 ];
 
-const startingLunarMonthInCurrentYear  = 1; 
+const startingLunarMonthInCurrentYear = 1; 
 
 const lunarYearOfBirthday = parseInt(prompt('your Lunar birth year: '));
 const lunarMonthOfBirthday = parseInt(prompt('your Lunar birth month: '));
 const lunarDayOfBirthday = parseInt(prompt('your Lunar birth day: '));
 
-const lunarAge = currentYear - lunarYearOfBirthday;
+const upcomingBirthdayYear = getUpcomingLunarBirthdayYear();
+const lunarAge = upcomingBirthdayYear - lunarYearOfBirthday;
+const daysSinceLunarNewYear = countDaysFromLunarNewYear(lunarMonthOfBirthday, lunarDayOfBirthday);
+//tuples
+const [solarMonthOfBirthday, solarDayOfBirthday] = getSolarBirthday(daysSinceLunarNewYear, startingLunarMonthInCurrentYear);
+showSolarBirthday();
 
-const allDaysSinceLunarNewYear = sumAllDays(lunarMonthOfBirthday, lunarDayOfBirthday); // lunarDaysSinceNewYear
 
-const solarBirthday = getSolarBirthday(allDaysSinceLunarNewYear, startingLunarMonthInCurrentYear);
-const solarDayOfBirthday = solarBirthday[1];
-const solarMonthOfBirthday = solarBirthday[0];
+function getUpcomingLunarBirthdayYear() {
+    if (lunarMonthOfBirthday == 12 && lunarDayOfBirthday > 9) {
+        return currentYear + 1;
+    } else {
+        return currentYear;
+    }
+}
 
-var ordinal = require('ordinal');
-
-console.log(`\n🎂 Your ${ordinal(lunarAge)} birthday is ${monthsInString[solarMonthOfBirthday]} ${solarDayOfBirthday}, ${currentYear}.\n`);
-
-function sumAllDays(month, day) {
-    let sum = 0;
-    for (let i = 0; i < month; i++) {
-        if (i == month - 1) {
-            sum += day;
+function countDaysFromLunarNewYear(lunarMonth, lunarDay) {
+    let days = 0;
+    for (let i = 0; i < lunarMonth; i++) {
+        if (i == lunarMonth - 1) {
+            days += lunarDay;
         } else {
-            sum += lunarDaysForEachMonthInCurrentYear[i];
+            days += lunarDaysForEachMonthInCurrentYear[i];
         }
     }
-    return sum;
+    return days;
 }
 
 function getSolarBirthday(allDays, startingLunarMonthInCurrentYear) {
     let solarDay = allDays;
-    let solarMonth = startingLunarMonthInCurrentYear;
+    let solarMonth;
 
-    while(solarDay > solarDaysForEachMonthInCurrentYear[solarMonth]) {
-        solarDay -= solarDaysForEachMonthInCurrentYear[solarMonth++];
+    for (solarMonth = startingLunarMonthInCurrentYear; solarMonth <= lunarMonthOfBirthday; solarMonth++) {
+        if (solarDay > solarDaysForEachMonthInCurrentYear[solarMonth]) {
+            solarDay -= solarDaysForEachMonthInCurrentYear[solarMonth];
+        } else {
+            break;
+        }
+    }
+
+    if (solarMonth == 12) {
+        solarMonth = 0;
     }
 
     return [solarMonth, solarDay];
+}
+
+function showSolarBirthday() {
+    process.stdout.write(`\n🎂 Your ${ordinal(lunarAge)} birthday `);
+
+    if (solarMonthOfBirthday == currentMonth && solarDayOfBirthday == currentDay) {
+        console.log("is Today!!");
+        console.log("😀😀 HAPPY BIRTHDAY! 😀😀\n")
+    } else {
+        if (solarMonthOfBirthday > currentMonth && solarDayOfBirthday > currentDay) {
+            process.stdout.write("is");
+        } else {
+            process.stdout.write("was");
+        }
+    
+        console.log(` ${monthsInString[solarMonthOfBirthday]} ${solarDayOfBirthday}, ${upcomingBirthdayYear}.\n`);
+    }
 }
