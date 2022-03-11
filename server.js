7bf9f37773fd69876bee33a1888de6c13e2a4d6a
Yujin Won <iamyujinwon@gmail.com>
2022-03-11 16:29:16 -0500

    refactor code using solarlunar

--- server.js
+++ server.js
@@ -4,106 +4,93 @@ const currentYear = new Date().getUTCFullYear();
 const currentMonth = new Date().getUTCMonth() + 1;
 const currentDay = new Date().getUTCDate();
 
-const b = getCurrentLunarDayFromSolarDate(2021,1,30);
-const d = getCurrentLunarDayFromLunarDate(1,30);
+const solar = getSolarDateInfo(1999,5,17);
+const lunar = getLunarDateInfo(1974,5,6);
 
-console.log(b);
+const specificDate = convertSolarToLunarInSpecificYear(2022, 1999, 5, 17);
+const specificDate2 = convertLunarToSolarInSpecificYear(2022, 4, 3);
 
-console.log(d);
+const list = getDateListFromRange(1999, 2022, 4, 3);
 
+console.log(solar);
+console.log(lunar);
+console.log(specificDate);
+console.log(specificDate2);
+console.log(list);
 
-// lunar -> solar in 2022
-// solar -> solar in 2022
-// specific year argument optional로 만약 넣으면, 그 해당 한해에 대한 거 알려주기
+//위 펑션들 안에서 이거 쓰기!!
+function convertSolarToLunarInSpecificYear (specificYear, solarYear, solarMonth, solarDay) {
+    const solarToLunar =  solarLunar.solar2lunar(solarYear, solarMonth, solarDay);
+    const specificDate =  solarLunar.lunar2solar(specificYear, solarToLunar.lMonth, solarToLunar.lDay);
 
+    return {
+        'year': specificYear, 
+        'month': specificDate.cMonth,
+        'day': specificDate.cDay
+    };
+}
 
-// my library
+//위 펑션들 안에서 이거 쓰기!!
+function convertLunarToSolarInSpecificYear (specificYear, lunarMonth, lunarDay) {
+    const specificDate =  solarLunar.lunar2solar(specificYear, lunarMonth, lunarDay);
 
-// 특정 날의 음력 날짜를 현재의 양력 날짜로 변환하여 알려준다
+    return {
+        'year': specificYear, 
+        'month': specificDate.cMonth,
+        'day': specificDate.cDay
+    };
+}
 
-
-// 만약, 유저가 특정 날짜의 양력 날짜를 넣었을 경우,
-// 그 양력 날짜를 먼저 그 해당 해의 음력 날짜로 변환한 후,
-// 변환된 음력 날짜를 가지고 현재 양력 날짜로 언제인지 알려준다
-// argument: Date object --> 다른 형태의 오브젝트들도 되는지 확인하기!!
-// argument: three arguments
-// solarToLunar(1999,05,17) or solarToLunar(1999,5,17)
-// output: 
-function getCurrentLunarDayFromSolarDate(solarYear, solarMonth, solarDay) {
+function getSolarDateInfo(solarYear, solarMonth, solarDay) {
     const solarToLunar = solarLunar.solar2lunar(solarYear, solarMonth, solarDay);
-    let lunarToSolar = solarLunar.lunar2solar(currentYear, solarToLunar.lMonth, solarToLunar.lDay);
-
-    if ((lunarToSolar.cMonth < currentMonth) || (lunarToSolar.cMonth == currentMonth && lunarToSolar.cDay < currentDay)) {
-        lunarToSolar = solarLunar.lunar2solar(currentYear, solarToLunar.lMonth, solarToLunar.lDay);
-    };
+    const lunarToCurrentSolar = convertLunarToSolarInSpecificYear(currentYear, solarToLunar.lMonth, solarToLunar.lDay)
+    const upcomingLunarToSolar = convertLunarToSolarInSpecificYear(currentYear + 1, solarToLunar.lMonth, solarToLunar.lDay)
 
     return {
-        'cYear': lunarToSolar.cYear,
-        'cMonth': lunarToSolar.cMonth,
-        'cDay': lunarToSolar.cDay
+        'sYear': solarYear,
+        'sMonth': solarMonth,
+        'sDay': solarDay,
+        'lYear': solarToLunar.lYear,
+        'lMonth': solarToLunar.lMonth,
+        'lDay': solarToLunar.lDay,
+        'cYear': lunarToCurrentSolar.year,
+        'cMonth': lunarToCurrentSolar.month,
+        'cDay': lunarToCurrentSolar.day,
+        'uYear': upcomingLunarToSolar.year,
+        'uMonth': upcomingLunarToSolar.month,
+        'uDay': upcomingLunarToSolar.day
     };
 }
 
-// 만약, 유저가 특정 날짜의 음력 날짜를 넣었을 경우,
-// 그 음력 날짜가 현재 양력 날짜로 언제인지 알려준다
-// lunarToSolar(04, 03) or lunarToSolar(4,3))
-function getCurrentLunarDayFromLunarDate(month, day) {
-    const lunarMonth = month; 
-    const lunarDay = day;
-
-    let lunarToSolar = solarLunar.lunar2solar(currentYear, lunarMonth, lunarDay);
-
-    console.log(lunarToSolar.cMonth);
-    console.log(currentMonth);
-    console.log(lunarToSolar.cDay);
-    console.log(currentDay);
-
-    if ((lunarToSolar.cMonth < currentMonth) || (lunarToSolar.cMonth == currentMonth && lunarToSolar.cDay < currentDay)) {
-        lunarToSolar = solarLunar.lunar2solar(currentYear, lunarMonth, lunarDay);
-    };
+function getLunarDateInfo(lunarYear, lunarMonth, lunarDay) {
+    const lunarToSolar = solarLunar.lunar2solar(lunarYear, lunarMonth, lunarDay);
+    const currentLunarToSolar = solarLunar.lunar2solar(currentYear, lunarMonth, lunarDay);
+    const upcomingLunarToSolar = solarLunar.lunar2solar(currentYear + 1, currentLunarToSolar.lMonth, currentLunarToSolar.lDay);
 
     return {
-        'cYear': lunarToSolar.cYear,
-        'cMonth': lunarToSolar.cMonth,
-        'cDay': lunarToSolar.cDay
+        'sYear': lunarToSolar.cYear, 
+        'sMonth': lunarToSolar.cMonth,
+        'sDay': lunarToSolar.cDay,
+        'lYear': lunarYear,
+        'lMonth': lunarMonth,
+        'lDay': lunarDay,
+        'cYear': currentLunarToSolar.cYear,
+        'cMonth': currentLunarToSolar.cMonth,
+        'cDay': currentLunarToSolar.cDay,
+        'uYear': upcomingLunarToSolar.cYear,
+        'uMonth': upcomingLunarToSolar.cMonth,
+        'uDay': upcomingLunarToSolar.cDay
     };
 }
 
-// solarToLunar(new Date('1999-05-17'))
-// output: 
-// function getCurrentLunarDayFromSolarDate(solarDate) {
-//     const solarYear = solarDate.getUTCFullYear();
-//     const solarMonth = solarDate.getUTCMonth() + 1; 
-//     const solarDay = solarDate.getUTCDate();
-//     const solarToLunar = solarLunar.solar2lunar(solarYear, solarMonth, solarDay);
-//     const lunarDate = new Date(solarToLunar.lYear, (solarToLunar.lMonth - 1), solarToLunar.lDay); 
-//     const lunarMonth = lunarDate.getUTCMonth() + 1; 
-//     const lunarDay = lunarDate.getUTCDate();
-//     let lunarToSolar = solarLunar.lunar2solar(currentYear, lunarMonth, lunarDay);
+// 구글 달력에 저장할때, 어느 년도 부터 어느 년도까지 저장하고 싶은지 정하고 저장 가능하게 만드는 기능!
+function getDateListFromRange(startingYear, endingYear, lunarMonth, lunarDay) {
+    let list = [];
 
-//     if ((lunarToSolar.cMonth < currentMonth) || (lunarToSolar.cMonth == currentMonth && lunarToSolar.cDay < currentDay)) {
-//         lunarToSolar = solarLunar.lunar2solar(currentYear, lunarMonth, lunarDay);
-//     } 
+    for (let i = startingYear; i <= endingYear; i++) {
+        const date =  solarLunar.lunar2solar(i, lunarMonth, lunarDay);
+        list.push({'year': date.cYear, 'month': date.cMonth, 'day': date.cDay})
+    }
 
-//     return new Date(lunarToSolar.cYear, (lunarToSolar.cMonth - 1), lunarToSolar.cDay); 
-// }
-
-
-// 만약, 유저가 특정 날짜의 음력 날짜를 넣었을 경우,
-// 그 음력 날짜가 현재 양력 날짜로 언제인지 알려준다
-// argument: Date object
-// lunarToSolar(new Date('1999-04-03'))
-// output: 
-// function getCurrentLunarDayFromLunarDate(lunarDate) {
-//     const lunarMonth = lunarDate.getUTCMonth() + 1; 
-//     const lunarDay = lunarDate.getUTCDate();
-
-//     let lunarToSolar = solarLunar.lunar2solar(currentYear, lunarMonth, lunarDay);
-
-//     if ((lunarToSolar.cMonth < currentMonth) || (lunarToSolar.cMonth == currentMonth && lunarToSolar.cDay < currentDay)) {
-//         lunarToSolar = solarLunar.lunar2solar(currentYear, birthdayMonth, birthdayDay);
-//     } 
-
-//     return new Date(lunarToSolar.cYear, (lunarToSolar.cMonth - 1), lunarToSolar.cDay); v
-// }
-
+    return list;
+}
