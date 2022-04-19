c1ea0066e364d967a578f7d1543af47a67168235
Yujin Won <iamyujinwon@gmail.com>
2022-04-19 14:05:46 -0400

    fix function typo

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const currentYear = new Date().getUTCFullYear();
 
 function fromSolarDate(solarYear, solarMonth, solarDay, targetYear = currentYear) {
     const solarToLunar = solarLunar.solar2lunar(solarYear, solarMonth, solarDay);
-    const lunarToCurrentSolar = solarLunar.lunar2solar(targetYear, solarToLunar.lMonth, solarToLunar.lDay);
+    const lunarToTargetSolar = solarLunar.lunar2solar(targetYear, solarToLunar.lMonth, solarToLunar.lDay);
 
     return {
-        'year': lunarToCurrentSolar.cYear,
-        'month': lunarToCurrentSolar.cMonth,
-        'day': lunarToCurrentSolar.cDay
+        'year': lunarToTargetSolar.cYear,
+        'month': lunarToTargetSolar.cMonth,
+        'day': lunarToTargetSolar.cDay
     };
 }
 
 function fromLunarDate(lunarYear, lunarMonth, lunarDay, targetYear = currentYear) {
-    const currentLunarToSolar = solarLunar.lunar2solar(targetYear, lunarMonth, lunarDay);
+    const targetLunarToSolar = solarLunar.lunar2solar(targetYear, lunarMonth, lunarDay);
 
     return {
-        'year': currentLunarToSolar.cYear,
-        'month': currentLunarToSolar.cMonth,
-        'day': currentLunarToSolar.cDay
+        'year': targetLunarToSolar.cYear,
+        'month': targetLunarToSolar.cMonth,
+        'day': targetLunarToSolar.cDay
     };
 }
 
